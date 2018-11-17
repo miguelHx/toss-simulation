83b60fcb25227c169afee804f6f0aa1412654d32
miguelhx <mhernandez401405@gmail.com>
2018-11-16 17:48:14 -0800

    add PageSlider for int range, setup visual slider for plot, rename dir

--- toss_simulation.py
+++ toss_simulation.py
@@ -5,8 +5,9 @@ What is the probability mass function of the total number of tails?  Start at ma
 """
 import matplotlib.patches as mpatches
 import matplotlib.pyplot as plt
-from helper_methods.stats import simulated_binomial_pmf
-from helper_methods.stats import get_theoretical_probabilities, get_simulated_probabilities
+from utils.PageSlider import PageSlider
+from utils.stats import simulated_binomial_pmf
+from utils.stats import get_theoretical_probabilities, get_simulated_probabilities
 
 
 def plot_coin_toss(n, k, p, t, simulations):
@@ -15,12 +16,17 @@ def plot_coin_toss(n, k, p, t, simulations):
 
     width = 10
     height = 7
-    plt.figure(figsize=(width, height))  # window size
+    fig = plt.figure(figsize=(width, height))  # window size
+    fig.subplots_adjust(bottom=0.2)
+
+    theoretical_binomial_pmf_equation = r"$P(X=k)=\binom{n}{k}((1-p)^t)^k(1-(1-p)^t)^{(n-k)}$"  # laTex
+    plt.title("Binomial Dist. Simulation: {}".format(theoretical_binomial_pmf_equation))
+    plt.xlabel("K tails")
+    plt.ylabel("Probability")
 
     # plot theoretical y
     y_theoretical = get_theoretical_probabilities(start, end, n, p, t)
     y_theoretical_line, = plt.plot(x, y_theoretical, 'b-', label='Theoretical', marker='o')
-
     # plot simulated y
     y_simulated = get_simulated_probabilities(start, end, n, p, t, simulations)
     y_simulated_line, = plt.plot(x, y_simulated, 'r--', label='Simulated (Experimental)', marker='o')
@@ -34,11 +40,37 @@ def plot_coin_toss(n, k, p, t, simulations):
     txt = "'k' tails out of 'n' coins, with a max of 't' tosses (RE-toss on tails)"
     plt.figtext(0.5, 0.015, txt, wrap=True, horizontalalignment='center', fontsize=12)
 
-    theoretical_binomial_pmf_equation = r"$P(X=k)=\binom{n}{k}((1-p)^t)^k(1-(1-p)^t)^{(n-k)}$"  # laTex
-    plt.title("Binomial Dist. Simulation: {}".format(theoretical_binomial_pmf_equation))
-    plt.xlabel("K tails")
-    plt.ylabel("Probability")
+    # setup sliders
+    num_pages = 10
+    tosses_slider_ax = fig.add_axes([0.1, 0.07, 0.8, 0.04])  # dimensions for slider
+    toss_slider = PageSlider(tosses_slider_ax, 't', num_pages, activecolor="orange")
+
+    def update(val):
+        fig.canvas.draw()
+        print("Slider changed.")
+
+    # toss_slider.on_changed(update)
     plt.show()
+
+
+
+    # fig, ax = plt.subplots()
+    # plt.subplots_adjust(left=0.25, bottom=0.25)
+    # t = np.arange(0.0, 1.0, 0.001)
+    # s = np.sin(6 * np.pi * t)
+    # l, = plt.plot(t, s, lw=2, color='red')
+    # plt.axis([0, 1, -1.2, 1.2])
+    #
+    # axfreq = plt.axes([0.25, 0.1, 0.65, 0.03], facecolor="lightblue")
+    # sfreq = Slider(axfreq, 'Freq', 0.1, 20.0, valinit=3)
+    #
+    # def update(val):
+    #     l.set_ydata(np.sin(2 * np.pi * val * t))
+    #     fig.canvas.draw_idle()
+    #
+    # sfreq.on_changed(update)
+    #
+    # plt.show()
 
 
 def simulate_coin_toss_tails(num_coins, k, p_tails, tosses, simulations):
@@ -57,7 +89,7 @@ def main():
     # result = binomial_pmf(n, k, p_tails, t)
     # print("Theoretical probability with new formula: {}".format(result))
     #
-    num_simulations = 500
+    num_simulations = 3000
     args = (n, k, p_tails, t, num_simulations)
     # print("Simulating coin toss with n={}, k={}, p_tails={}, t={}, and trials={}".format(*args))
     # simulate_coin_toss_tails(*args)
---
+++ utils/PageSlider.py
@@ -0,0 +1,107 @@
+import matplotlib.widgets
+import matplotlib.patches
+import mpl_toolkits.axes_grid1
+
+
+class PageSlider(matplotlib.widgets.Slider):
+
+    def __init__(self, ax, label, numpages = 10, valinit=0, valfmt='%1d',
+                 closedmin=True, closedmax=True,
+                 dragging=True, **kwargs):
+        self.facecolor = kwargs.get('facecolor',"w")
+        self.activecolor = kwargs.pop('activecolor',"b")
+        self.fontsize = kwargs.pop('fontsize', 10)
+        self.numpages = numpages
+
+        super(PageSlider, self).__init__(ax, label, 0, numpages,
+                            valinit=valinit, valfmt=valfmt, **kwargs)
+
+        self.poly.set_visible(False)
+        self.vline.set_visible(False)
+        self.pageRects = []
+        for i in range(numpages):
+            facecolor = self.activecolor if i==valinit else self.facecolor
+            r  = matplotlib.patches.Rectangle((float(i)/numpages, 0), 1./numpages, 1,
+                                transform=ax.transAxes, facecolor=facecolor)
+            ax.add_artist(r)
+            self.pageRects.append(r)
+            ax.text(float(i)/numpages+0.5/numpages, 0.5, str(i+1),
+                    ha="center", va="center", transform=ax.transAxes,
+                    fontsize=self.fontsize)
+        self.valtext.set_visible(False)
+
+        divider = mpl_toolkits.axes_grid1.make_axes_locatable(ax)
+        bax = divider.append_axes("right", size="5%", pad=0.05)
+        fax = divider.append_axes("right", size="5%", pad=0.05)
+        self.button_back = matplotlib.widgets.Button(bax, label=r'<',
+                        color=self.facecolor, hovercolor=self.activecolor)
+        self.button_forward = matplotlib.widgets.Button(fax, label=r'>',
+                        color=self.facecolor, hovercolor=self.activecolor)
+        self.button_back.label.set_fontsize(self.fontsize)
+        self.button_forward.label.set_fontsize(self.fontsize)
+        self.button_back.on_clicked(self.backward)
+        self.button_forward.on_clicked(self.forward)
+        self.on_changed(self._update)
+
+    def _update(self, event):
+        if type(event).__name__ != 'int':
+            super(PageSlider, self)._update(event)
+        i = int(self.val)
+        if i >= self.valmax:
+            return
+        self._colorize(i)
+
+    def _colorize(self, i):
+        for j in range(self.numpages):
+            self.pageRects[j].set_facecolor(self.facecolor)
+        self.pageRects[i].set_facecolor(self.activecolor)
+
+    def forward(self, event):
+        current_i = int(self.val)
+        i = current_i+1
+        if (i < self.valmin) or (i >= self.valmax):
+            return
+        self._colorize(i)
+        self.set_val(i)
+        # self._update(event)
+
+    def backward(self, event):
+        current_i = int(self.val)
+        i = current_i-1
+        if (i < self.valmin) or (i >= self.valmax):
+            return
+        self._colorize(i)
+        self.set_val(i)
+        # self._update(event)
+
+
+if __name__ == "__main__":
+    import numpy as np
+    from matplotlib import pyplot as plt
+
+    t = 10
+    num_pages = t
+    data = np.random.rand(9, 9, num_pages)
+
+    fig, ax = plt.subplots()
+    fig.subplots_adjust(bottom=0.25)
+
+    im = ax.imshow(data[:, :, 0], cmap='viridis', interpolation='nearest', origin="lower")
+
+    ax_slider = fig.add_axes([0.1, 0.07, 0.8, 0.04])
+    ax_slider2 = fig.add_axes([0.1, 0.02, 0.8, 0.04])
+    slider = PageSlider(ax_slider, 'Page', num_pages, activecolor="orange")
+    slider2 = PageSlider(ax_slider2, 'Size', data.shape[0], valinit=8, activecolor="yellow")
+
+    def update(val):
+        i = int(slider.val)
+        j = int(slider2.val)
+        im.set_data(data[:j+1,:j+1,i])
+        im.set_extent([-0.5,j+0.5,-0.5,j+0.5])
+        fig.canvas.draw()
+
+
+    slider.on_changed(update)
+    slider2.on_changed(update)
+
+    plt.show()
